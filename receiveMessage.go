ced496acb3603503a9d67d946c358508ed1c71bb
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-26 13:59:25 +0800

    Code update

---
+++ receiveMessage.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"net"
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+
+
+func main() {
+	
+	// listen on all interfaces
+	ln, _ := net.Listen("tcp", "localhost:8081")
+
+	// accept connection on port
+	conn, _ := ln.Accept()
+	
+	
+	for {	
+		var xmlmsg string 
+		xmlmsg = getMessage()
+		fmt.Println(xmlmsg)	
+		conn.Write([]byte(xmlmsg + "\n"))		
+	}
+	
+	
+}
+
+func getMessage() string {
+	var msg string	
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+	
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"workresult", // name
+		false,   // durable
+		false,   // delete when usused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)	
+	
+	go func() {
+		for d := range msgs {
+			//log.Printf("Received a message: %s", d.Body)
+			msg = string(d.Body[:])			
+			forever <- true
+		}
+
+	}()
+
+	//log.Printf(" [*] Waiting for messages. To exit press CTRL+C -cd")
+	<-forever
+	return msg
+}
