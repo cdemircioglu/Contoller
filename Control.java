3c7f5df35edfd48e6f1c2f5391a8c0e5ac881ac5
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-26 09:27:11 +0800

    Code update

--- Control.java
+++ Control.java
@@ -1,6 +1,6 @@
 //javac -cp "rabbitmq-client.jar:mysql-connector-java-5.1.39-bin.jar:." Control.java -Xlint:unchecked
 //java -cp "rabbitmq-client.jar:mysql-connector-java-5.1.39-bin.jar:." Control
-
+//java -cp "rabbitmq-client.jar:mysql-connector-java-5.1.39-bin.jar:." Control
 
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
