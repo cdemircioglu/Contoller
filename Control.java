868effbb381112c726671ceb8fe062116cadfcb3
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-10-31 15:20:02 +0800

    Code update

--- Control.java
+++ Control.java
@@ -306,6 +306,7 @@ public class Control {
 		    String MSISDNMessage = "<parameter><name>msisdn</name><value>"+ msisdn +"</value></parameter>"; //MSISDN XML
 		    String RunNumber = "<parameter><name>runnumber</name><value>" + runNumber + "</value></parameter>"; //runNumber XML
 		    String FinalMessage = xmlParameters.replace("</ShinnyParameters>", ""); //remove the close of xml
+		    FinalMessage = xmlParameters.replace("<ShinnyParameters>", "<ShinnyParameters><parameter><name>runtime</name><value>1000</value></parameter>"); //Run time is the time required to run TODO
 		    FinalMessage = FinalMessage + RunNumber + MSISDNMessage + "</ShinnyParameters>";   
 		    		    
 		    channel.basicPublish("", QUEUE_NAME, null, FinalMessage.getBytes("UTF-8"));
