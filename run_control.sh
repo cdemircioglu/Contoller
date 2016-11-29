da41d2ea50cbfe93c9ebfd92f6e20062ed180b53
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-11-29 17:45:46 +0800

    Code update

--- run_control.sh
+++ run_control.sh
@@ -1,3 +1,3 @@
-javac -cp "rabbitmq-client.jar:mysql-connector-java-5.1.39-bin.jar:." Control.java -Xlint:unchecked
+javac -cp "rabbitmq-client.jar:mysql-connector-java-5.1.39-bin.jar:."Control.java -Xlint:unchecked
 javac -cp "rabbitmq-client.jar:mysql-connector-java-5.1.39-bin.jar:." ControllerCheck.java -Xlint:unchecked
 java -cp "rabbitmq-client.jar:mysql-connector-java-5.1.39-bin.jar:." ControllerCheck
