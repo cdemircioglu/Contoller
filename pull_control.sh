d0da802a291fa6d1eb03f25f2f8d2771e4ab46e9
Cem DEMIRCIOGLU <c81021292@china.huawei.com>
2016-11-29 15:26:45 +0800

    Code update

--- pull_control.sh
+++ pull_control.sh
@@ -3,5 +3,5 @@ git -C ~/control/ stash drop
 git -C ~/control/ fetch origin
 git -C ~/control/ merge origin/master
 sed -i -e 's/workermsisdn/workermsisdn_test/g' Control.java
-sed -i -e 's/workermsisdn/worknumber_test/g' Control.java
-sed -i -e 's/workermsisdn/workorder_test/g' Control.java
+sed -i -e 's/worknumber/worknumber_test/g' Control.java
+sed -i -e 's/workorder/workorder_test/g' Control.java
